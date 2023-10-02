4ccf6db1e09ac845b76059339587646ad9d9a2e8
nomsayn <mmichela11@gmail.com>
2023-10-02 00:04:26 -0600

    First Migrations

--- portfolio/portfolio_app/admin.py
+++ portfolio/portfolio_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Student
 
-# Register your models here.
+# Register your models here so they can be edited in admin panel
+admin.site.register(Student)
--- portfolio/portfolio_app/models.py
+++ portfolio/portfolio_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+
+    #List of choices for major value in database, human readable name
+    MAJOR = (
+    ('CSCI-BS', 'BS in Computer Science'),
+    ('CPEN-BS', 'BS in Computer Engineering'),
+    ('BIGD-BI', 'BI in Game Design and Development'),
+    ('BICS-BI', 'BI in Computer Science'),
+    ('BISC-BI', 'BI in Computer Security'),
+    ('CSCI-BA', 'BA in Computer Science'),
+    ('DASE-BS', 'BS in Data Analytics and Systems Engineering'))
+
+    name = models.CharField(max_length=200)
+    email = models.CharField("UCCS Email", max_length=200)
+    major = models.CharField(max_length=200, choices=MAJOR, blank = False)
