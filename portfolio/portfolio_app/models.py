ff707da457a9ec1f5d84a03c9ad6dfb0c03c14c7
nomsayn <mmichela11@gmail.com>
2023-10-02 06:36:06 -0600

    Update student model to display Name field in Admin panel

--- portfolio/portfolio_app/models.py
+++ portfolio/portfolio_app/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.urls import reverse
 
 class Student(models.Model):
 
-    #List of choices for major value in database, human readable name
+    # List of choices for major value in database, human readable name
     MAJOR = (
     ('CSCI-BS', 'BS in Computer Science'),
     ('CPEN-BS', 'BS in Computer Engineering'),
@@ -15,3 +16,14 @@ class Student(models.Model):
     name = models.CharField(max_length=200)
     email = models.CharField("UCCS Email", max_length=200)
     major = models.CharField(max_length=200, choices=MAJOR, blank = False)
+
+    # Define default String to return the name for representing the Model object."
+    def __str__(self):
+        return self.name
+
+    # Returns the URL to access a particular instance of MyModelName.
+    # if you define this method then Django will automatically
+    # add a "View on Site" button to the model's record editing screens in the Admin site
+    def get_absolute_url(self):
+        return reverse('student-detail', args=[str(self.id)])
+
